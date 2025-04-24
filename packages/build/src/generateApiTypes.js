badd75f65f935bd169fe15205925108b1ce92130
Le Vivilet <levivlet@pm.me>
2025-04-24 20:09:46 +0200

    clean code

--- packages/build/src/generateApiTypes.js
+++ packages/build/src/generateApiTypes.js
@@ -3,47 +3,8 @@ import { readFile, writeFile } from 'node:fs/promises'
 import { join } from 'node:path'
 import { root } from './root.js'
 
-const RE_WORD = /\w+/
-
 const getActualContent = (content) => {
   return content
-  // const lines = content.split('\n')
-
-  // const newLines = []
-  // let state = 'default'
-  // for (const line of lines) {
-  //   switch (state) {
-  //     case 'default':
-  //       if (line.startsWith('export')) {
-  //         state = 'export'
-  //         newLines.push('export interface TestApi {')
-  //       } else {
-  //         newLines.push(line)
-  //       }
-  //       break
-  //     case 'export':
-  //       if (line.startsWith('};')) {
-  //         state = 'after-export'
-  //         newLines.push('  readonly expect: any')
-  //         newLines.push('  readonly Locator: (selector: string, options?: any) => any')
-  //         newLines.push('}')
-  //         newLines.push('')
-  //         newLines.push('export interface Test {')
-  //         newLines.push('  (api: TestApi): Promise<void>')
-  //         newLines.push('}')
-  //       } else {
-  //         const word = line.match(RE_WORD)
-  //         newLines.push(`  readonly ${word}: typeof ${word},`)
-  //       }
-  //       break
-  //     case 'after-export':
-  //       break
-  //     default:
-  //       break
-  //   }
-  // }
-  // newLines.push('\n')
-  // return newLines.join('\n')
 }
 
 export const generateApiTypes = async () => {
