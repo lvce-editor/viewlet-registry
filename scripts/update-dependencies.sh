fe48d51fd094490ebbd34c090ff460bda87cc100
Le Vivilet <levivilet@pm.me>
2026-06-22 23:26:57 +0200

    update

--- scripts/update-dependencies.sh
+++ scripts/update-dependencies.sh
@@ -16,7 +16,7 @@ fi
 
 function updateDependencies {
   echo "updating dependencies..."
-  OUTPUT=`ncu -u -x @types/node -x lerna`
+  OUTPUT=`ncu -u -x @types/node -x lerna -x @babel/preset-typescript`
   SUB='All dependencies match the latest package versions'
   if [[ "$OUTPUT" == *"$SUB"* ]]; then
     echo "$OUTPUT"
